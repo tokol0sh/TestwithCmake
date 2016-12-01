9f40ff19cfc9d7a103e88b5e8dc323a992b7ba50
Byron Engler <bee25@uclive.ac.nz>
2016-12-02 09:38:35 +1300

    Added a comport. Can read from the port and store in a buffer. Tested with virtual comport

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,17 @@
 #include "Serial/serial.h"
 
 int main(int argc, char** argv) {
-		printf("Hello World\n");
-		serial::Serial SerialPort("COM1", 115200, serial::Timeout::simpleTimeout(1000));
-		return 0;
+
+	std::vector<uint8_t> buffer;
+	printf("Hello World\n");
+	serial::Serial SerialPort("COM2", 115200, serial::Timeout::simpleTimeout(1000));
+	while (1) {
+		if (!SerialPort.available()) {
+			printf("No data\n");
+		}
+		else {
+			SerialPort.read(buffer, 1);
+		}
+	}
+	return 0;
 }
